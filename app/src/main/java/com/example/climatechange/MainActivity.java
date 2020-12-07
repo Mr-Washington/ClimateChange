d7788833b9489014ebcfc0cc59c67deae5d50cb5
michael <washingt@bu.edu>
2020-12-06 19:03:19 -0500

    UI design

--- app/src/main/java/com/example/climatechange/MainActivity.java
+++ app/src/main/java/com/example/climatechange/MainActivity.java
@@ -6,18 +6,39 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        WebView myWebView = (WebView) findViewById(R.id.webview);
-        myWebView.loadUrl("https://developer.android.com/guide/webapps/webview#java");
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openactivity_main2();
+                ;
+            }
+        });
     }
-}
+        public void openactivity_main2() {
+            Intent intent = new Intent(this, MainActivity2.class);
+            startActivity(intent);
+        }
+        //TextView textView1 = findViewById(R.id.textView1);
+        //textView1.setTextColor(Color.BLUE);
+        //WebView myWebView = (WebView) findViewById(R.id.webview);
+        //myWebView.loadUrl("https://developer.android.com/guide/webapps/webview#java");
+    }
+
 
